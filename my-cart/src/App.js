a942f09557d8941f214e169d82693f896b2c1e6c
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-15 16:44:25 +0530

    create a components

--- my-cart/src/App.js
+++ my-cart/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
-    
+      <Navbar />
+      <ProductList />
+      <Footer />
     </>
   );
 }
---
+++ my-cart/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {  
+ 
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+             myCart
+            </a>
+            
+          </div>
+        </nav>
+        </>
+   
+    );
+}
+
+
+export default Navbar;
